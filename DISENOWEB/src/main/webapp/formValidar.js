38d909fd02f15b7fe794f6f0d62db41cc52108e1
Aaron Iglesias <38076669+AaronIglesiasChorro@users.noreply.github.com>
2021-01-20 00:04:31 +0100

    Create formValidar.js

---
+++ DISENOWEB/src/main/webapp/formValidar.js
@@ -0,0 +1,431 @@
+function validarAnadirEmpresa() {
+    var alerta = "Corrija los siguientes campos: \n";
+    var ok = true;
+    try {
+        var uno = Integer.parseInt(document.getElementById("idEmpresa"));
+    } catch (error) {
+        alerta += "   Los datos recibidos no son números enteros\n";
+        ok = false;
+    }
+
+    if(!validarLongitud(document.getElementById("idEmpresa").value,11)){
+        alerta += "  Id demasiado largo\n";
+        ok = false;
+    }
+    if(!validarLongitud(document.getElementById("nombre").value,45)){
+        alerta += "  Nombre demasiado largo\n";
+        ok = false;
+    }
+    
+    if(!validarLongitud(document.getElementById("calle").value,45)){
+        alerta += "  Nombre demasiado largo\n";
+        ok = false;
+    }
+    try {
+        var uno = Integer.parseInt(document.getElementById("codigo"));
+    } catch (error) {
+        alerta += "  -El telefono introducido no es un numero entero\n";
+        ok = false;
+    }
+    if(!validarLongitud(document.getElementById("codigo").value,5)){
+        alerta += "  -Telefono demasiado largo\n";
+        ok = false;
+    }
+
+    if(!validarEmail(document.getElementById("correo").value) && !validarLongitud(document.getElementById("correo").value,45)){
+        alerta += "  Correo invalido(demasiado largo o incorrecto)\n";
+        ok = false;
+    }
+    try {
+        var uno = Integer.parseInt(document.getElementById("telefono"));
+    } catch (error) {
+        alerta += "  -El telefono introducido no es un numero entero\n";
+        ok = false;
+    }
+    if(!validarLongitud(document.getElementById("telefono").value,11)){
+        alerta += "  -Telefono demasiado largo\n";
+        ok = false;
+    }
+
+    if(ok == true){
+        alerta += "  Todo OK!!"
+    }
+}
+
+function validarAnadirProyecto() {
+    var alerta = "Corrija los siguientes campos: \n";
+    var ok = true;
+    try {
+        var uno = Integer.parseInt(document.getElementById("idProyecto"));
+    } catch (error) {
+        alerta += "   Los datos recibidos de idProyecto no son números enteros\n";
+        ok = false;
+    }
+
+    if(!validarLongitud(document.getElementById("idProyecto").value,9)){
+        alerta += "  Id demasiado largo\n";
+        ok = false;
+    }
+    
+    try {
+        var uno = Integer.parseInt(document.getElementById("idEmpresa"));
+    } catch (error) {
+        alerta += "   Los datos recibidos de idEmpresa no son números enteros\n";
+        ok = false;
+    }
+
+    if(!validarLongitud(document.getElementById("idEmpresa").value,4)){
+        alerta += "  Id demasiado largo\n";
+        ok = false;
+    }
+
+    if(ok == true){
+        alerta += "  Todo OK!!"
+    }
+}
+
+function validarDarAlta() {
+    var alerta = "Corrija los siguientes campos: \n";
+    var ok = true;
+    if(!validarLongitud(document.getElementById("nombre").value,45)){
+        alerta += "  -Nombre demasiado largo\n";
+        ok = false;
+    }
+    if(!validarLongitud(document.getElementById("apellidos").value,45)){
+        alerta += "  -Apellidos demasiado largos\n";
+        ok = false;
+    }
+    try {
+        var uno = Integer.parseInt(document.getElementById("telefono"));
+    } catch (error) {
+        alerta += "  -El telefono introducido no es un numero entero\n";
+        ok = false;
+    }
+    if(!validarLongitud(document.getElementById("telefono").value,11)){
+        alerta += "  -Telefono demasiado largo\n";
+        ok = false;
+    }
+    if(!validarEmail(document.getElementById("correo").value) && !validarLongitud(document.getElementById("correo").value,45)){
+        alerta += "  -Correo invalido(demasiado largo o incorrecto)\n";
+        ok = false;
+    }
+    if(!validarLongitud(document.getElementById("contraseña").value,45)){
+        alerta += "  -Contraseña demasiado larga\n";
+        ok = false;
+    }
+    if(ok == true){
+        alerta += "  Todo OK!!"
+    }
+}
+
+function validarDarBaja() {
+    var alerta = "Corrija los siguientes campos: \n";
+    var ok = true;
+    try {
+        var uno = Integer.parseInt(document.getElementById("idUsuario"));
+    } catch (error) {
+        alerta += "   Los datos recibidos no son números enteros\n";
+        ok = false;
+    }
+
+    if(!validarLongitud(document.getElementById("idUsuario").value,11)){
+        alerta += "  Id demasiado largo\n";
+        ok = false;
+    }
+    if(!validarLongitud(document.getElementById("nombre").value,45)){
+        alerta += "  Nombre demasiado largo\n";
+        ok = false;
+    }
+    if(!validarLongitud(document.getElementById("apellidos").value,45)){
+        alerta += "  Apellidos demasiado largos\n";
+        ok = false;
+    }
+    if(!validarEmail(document.getElementById("correo").value) && !validarLongitud(document.getElementById("correo").value,45)){
+        alerta += "  Correo invalido(demasiado largo o incorrecto)\n";
+        ok = false;
+    }
+    if(ok == true){
+        alerta += "  Todo OK!!"
+    }
+}
+
+function validarDarBajaNav() {
+    var alerta = "Corrija los siguientes campos: \n";
+    var ok = true;
+    
+    if(!validarEmail(document.getElementById("correo").value) && !validarLongitud(document.getElementById("correo").value,45)){
+        alerta += "  Correo invalido(demasiado largo o incorrecto)\n";
+        ok = false;
+    }
+    if(ok == true){
+        alerta += "  Todo OK!!"
+    }
+}
+
+function validardiaLibre() {
+    var alerta = "Corrija los siguientes campos: \n";
+    var ok = true;
+    
+    if(!validarLongitud(document.getElementById("motivo").value,255)){
+        alerta += "  Exceso de caracteres\n";
+        ok = false;
+    }
+    
+    if(ok == true){
+        alerta += "  Todo OK!!"
+    }
+}
+
+function validarEditarUsuario() {
+    var alerta = "Corrija los siguientes campos: \n";
+    var ok = true;
+
+    try {
+        var uno = Integer.parseInt(document.getElementById("idUsuario"));
+    } catch (error) {
+        alerta += "  -El Id introducido no es un numero entero\n";
+        ok = false;
+    }
+
+    if(!validarLongitud(document.getElementById("idUsuario").value,2)){
+        alerta += "  -Id demasiado largo\n";
+        ok = false;
+    }
+
+    if(!validarLongitud(document.getElementById("nombre").value,45)){
+        alerta += "  -Nombre demasiado largo\n";
+        ok = false;
+    }
+    if(!validarLongitud(document.getElementById("apellidos").value,45)){
+        alerta += "  -Apellidos demasiado largos\n";
+        ok = false;
+    }
+
+    try {
+        var uno = Integer.parseInt(document.getElementById("telefono"));
+    } catch (error) {
+        alerta += "  -El telefono introducido no es un numero entero\n";
+        ok = false;
+    }
+
+    if(!validarLongitud(document.getElementById("telefono").value,11)){
+        alerta += "  -Telefono demasiado largo\n";
+        ok = false;
+    }
+
+    if(!validarEmail(document.getElementById("correo").value) && !validarLongitud(document.getElementById("correo").value,45)){
+        alerta += "  -Correo invalido(demasiado largo o incorrecto)\n";
+        ok = false;
+    }
+
+    if(!validarLongitud(document.getElementById("contraseña").value,4)){
+        alerta += "  -Contraseña demasiado larga\n";
+        ok = false;
+    }
+
+    if(ok == true){
+        alerta += "  Todo OK!!"
+    }
+}
+
+function validarEliminarEmpresa() {
+    var alerta = "Corrija los siguientes campos: \n";
+    var ok = true;
+    try {
+        var uno = Integer.parseInt(document.getElementById("idEmpresa"));
+    } catch (error) {
+        alerta += "   Los datos recibidos no son números enteros\n";
+        ok = false;
+    }
+
+    if(!validarLongitud(document.getElementById("idEmpresa").value,11)){
+        alerta += "  Id demasiado largo\n";
+        ok = false;
+    }
+    if(!validarLongitud(document.getElementById("nombre").value,45)){
+        alerta += "  Nombre demasiado largo\n";
+        ok = false;
+    }
+
+    if(!validarEmail(document.getElementById("correo").value) && !validarLongitud(document.getElementById("correo").value,45)){
+        alerta += "  Correo invalido(demasiado largo o incorrecto)\n";
+        ok = false;
+    }
+    try {
+        var uno = Integer.parseInt(document.getElementById("telefono"));
+    } catch (error) {
+        alerta += "  -El telefono introducido no es un numero entero\n";
+        ok = false;
+    }
+    if(!validarLongitud(document.getElementById("telefono").value,11)){
+        alerta += "  -Telefono demasiado largo\n";
+        ok = false;
+    }
+
+    if(ok == true){
+        alerta += "  Todo OK!!"
+    }
+}
+
+
+function validareliminarProyecto() {
+    var alerta = "Corrija los siguientes campos: \n";
+    var ok = true;
+    try {
+        var uno = Integer.parseInt(document.getElementById("idProyecto"));
+    } catch (error) {
+        alerta += "   Los datos recibidos de idProyecto no son números enteros\n";
+        ok = false;
+    }
+
+    if(!validarLongitud(document.getElementById("idProyecto").value,9)){
+        alerta += "  Id demasiado largo\n";
+        ok = false;
+    }
+    
+    if(!validarLongitud(document.getElementById("nombre").value,45)){
+        alerta += "  Nombre de empresa demasiado largo\n";
+        ok = false;
+    }
+
+    if(ok == true){
+        alerta += "  Todo OK!!"
+    }
+}
+
+function validarFichar() {
+    var alerta = "Corrija los siguientes campos: \n";
+    var ok = true;
+    try {
+        var uno = Integer.parseInt(document.getElementById("proyecto"));
+    } catch (error) {
+        alerta += "   Los datos recibidos de idProyecto no son números enteros\n";
+        ok = false;
+    }
+
+    if(!validarLongitud(document.getElementById("proyecto").value,9)){
+        alerta += "  Id demasiado largo\n";
+        ok = false;
+    }
+    
+    if(!validarLongitud(document.getElementById("nombre").value,45)){
+        alerta += "  Nombre de empresa demasiado largo\n";
+        ok = false;
+    }
+
+    if(ok == true){
+        alerta += "  Todo OK!!"
+    }
+}
+
+function validarIndex() {
+    var alerta = "Corrija los siguientes campos: \n";
+    var ok = true;
+    if(!validarEmail(document.getElementById("usuario").value) && !validarLongitud(document.getElementById("usuario").value,45)){
+        alerta += "  Correo invalido(demasiado largo o incorrecto)\n";
+        ok = false;
+    }
+    try {
+        var uno = Integer.parseInt(document.getElementById("password"));
+    } catch (error) {
+        alerta += "   Los datos recibidos de password no son números enteros\n";
+        ok = false;
+    }
+
+    if(!validarLongitud(document.getElementById("password").value,4)){
+        alerta += "  Id demasiado largo\n";
+        ok = false;
+    }
+    if(ok == true){
+        alerta += "  Todo OK!!"
+    }
+}
+function validarPC() {
+
+    var ok = true;
+    var alerta = "Corrija los siguientes campos: \n";
+
+    var email = document.getElementById("correo").value;
+    var texto = document.getElementById("presupuesto").value;
+
+    if(!validarEmail(email)){
+        ok = false;
+        alerta += "  -Email no válido\n";
+    }
+
+    if(!validarLongitud(texto, 500)){
+        ok = false;
+        alerta += "  -Texto excede carácteres máximos\n";
+    }
+
+    if(!ok){
+        alert(alerta);
+    }
+}
+
+
+function validarOpina(){
+
+    var ok = true;
+    var alerta = "Corrija los siguientes campos: \n";
+
+    var texto = document.getElementById("opinion").value;
+
+    if(!validarLongitud(texto, 500)){
+        ok = false;
+        alerta += "  -Texto excede carácteres máximos\n";
+    }
+
+    if(!ok){
+        alert(alerta);
+    }
+}
+
+function validarQueja(){
+
+    var ok = true;
+    var alerta = "Corrija los siguientes campos: \n";
+
+    var email = document.getElementById("correo").value;
+    var texto = document.getElementById("mensaje").value;
+    var eleccion = document.getElementById("razon").value;
+
+    if(!validarEmail(email)){
+        ok = false;
+        alerta += "  -Email no válido\n";
+    }
+
+    if(!validarLongitud(texto, 500)){
+        ok = false;
+        alerta += "  -Texto excede carácteres máximos\n";
+    }
+
+    if(eleccion == 0){
+        ok = false;
+        alerta += "  -Elija un motivo\n";
+    }
+
+    if(!ok){
+        alert(alerta);
+    }
+
+}
+
+function validarEmail(valor) {
+
+    emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+
+    if (emailRegex.test(valor)){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validarLongitud(valor, longitud){
+    if(valor.length <= longitud){
+        return true;
+    } else {
+        return false;
+    }
+}
